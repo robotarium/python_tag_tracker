3b45b195155e075b0e3e61ca689265b9e9c4437d
pglotfel <pglotfel@gmail.com>
2019-05-14 16:25:17 -0400

    Currently broken.  Integrating Vizier

--- tag_tracker/tracker.py
+++ tag_tracker/tracker.py
@@ -9,6 +9,7 @@ import threading
 import queue
 import yaml
 import json
+import concurrent.futures as futures
 
 from tag_tracker.utils import *
 
@@ -23,6 +24,36 @@ VISIBLE_COLOR = (255, 255, 255)
 FRAME_QUEUE = queue.Queue()
 RUNNING = True
 TIMEOUT = 5
+
+def get_data_task(tracker_node, ids, poses, running):
+
+    executor = futures.ThreadPoolExecutor(max_workers=len(ids))
+    links = list([i+'/status' for i in ids])
+
+    while running:
+        current_time = time.time()
+        data = []
+        try:
+            data = list(executor.map(lambda x: tracker_node.get(x, timeout=0.5, attempts=4), links, timeout=3))
+        except Exception as e:
+            print(repr(e))
+
+        for i, d in enumerate(data):
+            tag = ids[i]
+            print(tag in poses)
+            if tag in poses and d is not None:
+                try:
+                    d = json.loads(d)
+                    poses[tag].update(d)
+                    print('UPDATED')
+                except Exception as e:
+                    print(repr(e))
+
+            print(data)
+            # TODO: Make this sleep for at most 2 seconds
+            time.sleep(max(0, 2 - (time.time() - current_time)))
+
+    print('Get data task stopped')
 
 
 def read_from_camera(cap):
@@ -110,9 +141,21 @@ def main():
     t = threading.Thread(target=read_from_camera, args=(cap,))
     t.start()
 
+
+    possible_ids = {x.split('/')[0] for x in tracker_node.gettable_links}
+    print(possible_ids)
+    # TODO: Keep track of whether the ID is being tracker and whether it's responding to GET
+    # TODO: We can send messages based on that
+    poses = {}
+
+    get_data_thread_running = True
+    get_data_thread = threading.Thread(target=get_data_task, args=(tracker_node, list(possible_ids), poses, get_data_thread_running))
+    get_data_thread.start()
+
     # Start MQTT client
     #mqtt_client.start()
     tracker_node.start()
+
 
     # Initialize exponential filter for FPS
     avg_proc_time = 0.033
@@ -145,18 +188,15 @@ def main():
         # I can do what I want with corners now
         aruco.drawDetectedMarkers(gray, corners, ids, VISIBLE_COLOR)
 
-        #if(len(corners) > 0):
-        #    for i in range(ids.shape[0]):
-        #        # Make sure this id isn't a reference marker
-        #        if(ids[i][0] in reference_markers):
-        #            continue
-        #        
-        #        cv.undistortPoints(corners[i], cam_matrix, dist_coeffs, dst=corners[i], P=proj_matrix)
-        
-        poses = {}
+        # Determine poses of robots from image coordinates
         if(len(corners) > 0):
             for i in range(ids.shape[0]):
                 tag_id = ids[i][0]
+                tag_id_str = repr(tag_id)
+
+                if(tag_id_str not in possible_ids):
+                    print('Got id:', tag_id, 'not in possible ids (see node descriptor)')
+                    continue
 
                 if(tag_id in reference_markers):
                     continue
@@ -178,13 +218,13 @@ def main():
 
                 # TODO: What is this?
                 forward_vector = 0.5*(tag_pos[1][0] + tag_pos[2][0] - tag_pos[0][0] - tag_pos[3][0])
-
-                poses[repr(tag_id)] = {
+        
+                # Assemble poses JSON in the assumed format
+                # TODO: If I want to share this between threads, I can't re-init the dict.  It blows away the other changes
+                poses[tag_id_str] = {
                         'x': round(float(position[0]), 2),
                         'y': round(float(position[1]), 2),
                         'theta': round(float(np.math.atan2(forward_vector[1], forward_vector[0])), 2),
-                        'batt_volt': -1,
-                        'charge_status': False,
                         }
 
         # Send poses
@@ -216,11 +256,14 @@ def main():
             break
 
     # Stop MQTT client
-    mqtt_client.stop() 
+    #mqtt_client.stop() 
+    tracker_node.stop()
 
     # Terminate the thread that's running in the background
+    get_data_thread_running = False
     RUNNING = False
     t.join()
+    get_data_thread.join()
     cap.release()
     cv.destroyAllWindows()
 
