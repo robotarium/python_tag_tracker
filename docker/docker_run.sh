a94b95d097cebac9284176806467db1691ccfd0b
pglotfel <pglotfel@gmail.com>
2019-05-16 13:57:52 -0400

    updates

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker run --rm -ti \
+docker run -d \
+	--name robotarium_tag_tracker \
         -e DISPLAY=$DISPLAY \
         -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
         --device /dev/video0:/dev/video0 \
