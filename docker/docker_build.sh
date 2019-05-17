ff5edc0e4229b8995536d617f980ffb0173350dd
pglotfel <pglotfel@gmail.com>
2019-05-17 15:28:56 -0400

    adding sudo to docker build

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -11,7 +11,7 @@ if [ "$2" == "" ]; then
 fi
 
 
-docker build --tag robotarium:python_tag_tracker \
-	--build-arg ROBO_HOST="$1" \
-	--build-arg ROBO_PORT="$2" \
-	.
+sudo docker build --tag robotarium:python_tag_tracker \
+		  --build-arg ROBO_HOST="$1" \
+		  --build-arg ROBO_PORT="$2" \
+		  .
--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker run -d \
-	--name robotarium_tag_tracker \
-        -e DISPLAY=$DISPLAY \
-        -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-        --device /dev/video0:/dev/video0 \
-        robotarium:python_tag_tracker
+sudo docker run -d \
+		--name robotarium_tag_tracker \
+        	-e DISPLAY=$DISPLAY \
+        	-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+        	--device /dev/video0:/dev/video0 \
+        	robotarium:python_tag_tracker
